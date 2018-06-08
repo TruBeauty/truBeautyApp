6dfeb4fb73770a9b11dc0702193bd153ea8bd6f9
sannasuriya <pasindu20sliit@gmail.com>
2018-06-08 19:02:26 +0530

    d
    delete

--- app/src/main/java/srilanka/sliit/apps/trubeauty/MainActivity.java
+++ app/src/main/java/srilanka/sliit/apps/trubeauty/MainActivity.java
@@ -13,45 +13,45 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     sqLite_DB db;
-    String name,number,email,gender;
+    String name, number, email, gender;
     Button btn;
 
-    TextView tv1,tv2;
+    TextView tv1, tv2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        db=new sqLite_DB(getApplicationContext());
+        db = new sqLite_DB(getApplicationContext());
 
-        Cursor data=db.getUserTable();
+        Cursor data = db.getUserTable();
 
-        tv1=(TextView)findViewById(R.id.tv_welcome);
-        tv2=(TextView)findViewById(R.id.tv_details);
-        btn=(Button)findViewById(R.id.btn_loginback);
+        tv1 = (TextView) findViewById(R.id.tv_welcome);
+        tv2 = (TextView) findViewById(R.id.tv_details);
+        btn = (Button) findViewById(R.id.btn_loginback);
 
 
-        if (data != null){
+        if (data != null) {
             data.moveToFirst();
 
-            number=data.getString(1);
-            name=data.getString(2);
-            email=data.getString(3);
-            gender=data.getString(4);
+            number = data.getString(1);
+            name = data.getString(2);
+            email = data.getString(3);
+            gender = data.getString(4);
 
         }
 
-        tv1.setText("Welcome "+name+"!");
-        tv2.setText("UserID : "+number
-        +"\nEmail : "+email
-        +"\nGender : "+gender);
+        tv1.setText("Welcome " + name + "!");
+        tv2.setText("UserID : " + number
+                + "\nEmail : " + email
+                + "\nGender : " + gender);
 
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(getApplicationContext(),Welcome_Screen_Activity.class);
+                Intent intent = new Intent(getApplicationContext(), Welcome_Screen_Activity.class);
                 startActivity(intent);
                 finish();
             }
@@ -83,27 +83,4 @@ public class MainActivity extends AppCompatActivity {
         alertDialog.show();
     }
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.main);
 
-            Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-            setSupportActionBar(toolbar);
-
-            ViewPager viewPager = (ViewPager) findViewById(R.id.viewPager);
-            setupViewPager(viewPager);
-
-            TabLayout tabLayout = (TabLayout) findViewById(R.id.tabLayout);
-            tabLayout.setupWithViewPager(viewPager);//setting tab over viewpager
-        }
-
-        //Setting View Pager
-        private void setupViewPager(ViewPager viewPager) {
-            ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
-            adapter.addFrag(new Register_Salon(), "services");
-            viewPager.setAdapter(adapter);
-        }
-
-
-    }
